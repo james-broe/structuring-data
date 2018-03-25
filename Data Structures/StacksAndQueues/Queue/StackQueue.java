6829799bb0a2d3622e1619020101998991c0930b
James Broe <jbroe100@gmail.com>
2018-03-25 21:25:23 +1100

    changed folders

---
+++ Data Structures/StacksAndQueues/Queue/StackQueue.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class StackQueue<E> {
+
+  Stack myStack;
+  Stack helpStack;
+
+  public StackQueue() {
+    this.myStack = new Stack();
+    this.helpStack = new Stack();
+  }
+
+
+
+
+
+
+}
